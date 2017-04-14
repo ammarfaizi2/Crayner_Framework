d43f4e1ee09e93f04fdad8acc2ce4d247131e0bd
U0_a147 <ammarfaizi2@gmail.com>
2017-04-15 02:54:30 +0700

    Optimize memory with short circuit

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
 require 'loader.php';
 require 'Config/Config.php';
-$a = new System\URI_Segment();
-$a = new System\Action_Handler($a->gs());
-$a->run();
+(new System\Action_Handler((new System\URI_Segment())->gs()))->run();
