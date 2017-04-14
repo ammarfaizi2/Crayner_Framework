065ec0f43b7eb4a95955b0e7f8840c0ff541e9ee
U0_a147 <ammarfaizi2@gmail.com>
2017-04-15 01:42:20 +0700

    Optimize memory with overwrite variable

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 require 'loader.php';
 require 'Config/Config.php';
 $a = new System\URI_Segment();
-$b = new System\Action_Handler($a->gs());
-$b->run();
+$a = new System\Action_Handler($a->gs());
+$a->run();
