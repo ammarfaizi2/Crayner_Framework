5e906b3d75da89c1f6f19faeda4efee9c5afc6c8
U0_a147 <ammarfaizi2@gmail.com>
2017-04-15 01:02:32 +0700

    Fix URI Segment

--- System/URI_Segment.php
+++ System/URI_Segment.php
@@ -6,7 +6,8 @@ class URI_Segment extends Core
 {
 	public function gs()
 	{
-		$a = explode('/',$_SERVER['REQUEST_URI']);
+		$a = explode('?',$_SERVER['REQUEST_URI']);
+		$a = explode('/',$a[0]);
 		return $a;
 	}
 }
