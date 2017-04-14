1b8be524ff4c22abc2b56b1d7a09ad59e299ff69
U0_a147 <ammarfaizi2@gmail.com>
2017-04-15 00:26:10 +0700

    Create register model

---
+++ App/Models/register.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Models;
+use System\Model;
+
+class register extends Model
+{
+	public function __construct()
+	{
+		parent::db();
+	}
+	public function validation()
+	{
+		
+	}
+	public function errorInfo()
+	{
+		
+	}
+	public function insert()
+	{
+		
+	}
+}
