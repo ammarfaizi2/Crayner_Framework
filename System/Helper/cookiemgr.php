6e2f7a41839d54281b627d87ba93b37c2b9ce753
U0_a147 <ammarfaizi2@gmail.com>
2017-04-15 10:04:46 +0700

    Create cookiemgr helper

---
+++ System/Helper/cookiemgr.php
@@ -0,0 +1,22 @@
+<?php
+if(!function_exists('stcookie')){
+	function stcookie($ck,$ev='+')
+	{
+		$tm = time();
+		foreach($ck as $key => $val){
+			$aa[] = setcookie($key,$val[0],($ev=="+"?$tm+$val[1]:$tm-$val[1]));
+		}
+		return $aa;
+	}
+}
+if(!function_exists('clrcookie')){
+	function clrcookie($exception=null){
+		$except = $exception===null?array():explode(',');
+		foreach($_COOKIE as $key => $val){
+			if(!in_array($key,$except)){
+				$aa[]=setcookie($key,null,0);
+			}
+		}
+		return $aa;
+	}
+}
