9f75a6a2cc3db98583268c4342542783e5c127c8
U0_a147 <ammarfaizi2@gmail.com>
2017-04-21 13:07:31 +0700

    Modif Config

--- Config/Config.php
+++ Config/Config.php
@@ -2,3 +2,10 @@
 define('BASEURL','http'.(isset($_SERVER['HTTPS'])?'s':'').'://'.$_SERVER['HTTP_HOST']);
 define('BASEPATH',realpath(__DIR__.'/..'));
 define('BSR',BASEURL.'/index.php');
+define('CORE_ENGINE',true);
+
+ini_set("max_execution_time",500);
+ini_set("memory_limit","4G");
+ini_set("display_errors",true);
+set_time_limit(500);
+ignore_user_abort(false);
