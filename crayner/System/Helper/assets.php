6c1632d29b82d2500ef82d3cc4d0aff2565422b2
U0_a147 <ammarfaizi2@gmail.com>
2017-04-29 11:00:46 +0700

    Create assets helper

---
+++ crayner/System/Helper/assets.php
@@ -0,0 +1,13 @@
+<?php
+if(!function_exists('js')){
+	function js($file,$abs=false)
+	{
+		print '<script type="text/javascript" src="'.(BASEURL.JSURL).'/'.($abs?$file:$file.'.js').'">';
+	}
+}
+if(!function_exists('css')){
+	function css($file,$abs=false)
+	{
+		print '<link rel="stylesheet" type="text/css" href="'.(BASEURL.CSSURL).'/'.($abs?$file:($file.'.css').'">';
+	}
+}
