8020d2b577e1370d097447933148f957e985ea1f
U0_a147 <ammarfaizi2@gmail.com>
2017-04-30 09:42:45 +0700

    Make 'make' method

--- crayner/Console/Action_Handler.php
+++ crayner/Console/Action_Handler.php
@@ -8,9 +8,21 @@ class Action_Handler
 	{
 		$this->cmd = $cmd;
 	}
+	public function make($type,$filename)
+	{
+		switch($type){
+			case 'model':
+				$tpl = __DIR__.'/Repository/model.ice'
+				if(!file_exists($tpl))
+				$a = str_replace("•••model•••",$filename,file_get_contents($tpl));
+		}
+	}
+	private function ckfile($file)
+	{
+		file_exists($file) or die("\n\nError !\nCould not open input file: ".$file."\n\n");
+	}
 	public function run()
 	{
-		print_r($this);
 		switch($this->cmd[1]){
 		case 'make:controller':
 			return $this->make('controller',$this->cmd[2]);
--- crayner/Console/Console_Handler.php
+++ crayner/Console/Console_Handler.php
@@ -5,6 +5,7 @@ class Console_Handler
 {
 	public function __construct($ar)
 	{
+		$ar[0]=='icetea' or die("\nError verify icetea file !\n");
 		$this->handler = new Action_Handler($ar);
 		$this->action = $this->handler->run();
 	}
