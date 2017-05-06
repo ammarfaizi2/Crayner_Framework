d97d5489fc0462f2e9b3f968696236d8163e93e0
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-06 12:05:32 +0700

    Modif color

--- crayner/Console/ConsoleHandler.php
+++ crayner/Console/ConsoleHandler.php
@@ -2,11 +2,12 @@
 namespace Console;
 
 use Console\ActionHandler;
-
+use Console\Color\Colors;
 class ConsoleHandler
 {
     public function __construct($ar)
     {
+        $this->clr = new Colors();
         $ar[0]=='icetea' or die("\nError verify icetea file !\n");
         count($ar)==1 and $this->info();
         $this->handler = new ActionHandler($ar);
@@ -14,6 +15,7 @@ class ConsoleHandler
     }
     private function info()
     {
+        print $this->clr->getColoredString("halo","red");
         die;
     }
     public function run()
